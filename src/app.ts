798ac251b518fd48e0c2fc9e84a97c9761ef418f
saifur2233 <saifurnstuiit223344@gmail.com>
2023-06-02 09:00:25 +0600

    [Done] setup express, mongoose, cors, parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
 const app: Application = express();
 
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //Testing
 app.get("/", async (req: Request, res: Response) => {
   res.send("Working Successfully");
