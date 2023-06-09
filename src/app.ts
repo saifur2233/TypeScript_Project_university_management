c1be716eccbd33b3e7278738030dcb152479ecab
saifur2233 <saifurnstuiit223344@gmail.com>
2023-06-09 11:28:17 +0600

    [Done] error handling, schema validation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-import usersRouter from './app/modules/users/users.route'
+import { UserRoutes } from './app/modules/users/user.route'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 
 const app: Application = express()
 
@@ -12,10 +13,13 @@ app.use(express.urlencoded({ extended: true }))
 
 // Application routes
 
-app.use('/api/v1/users/', usersRouter)
+app.use('/api/v1/users/', UserRoutes)
 //Testing
 app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully')
 })
+
+//global error handler
+app.use(globalErrorHandler)
 
 export default app
--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -1,6 +1,6 @@
-import { User } from './users.model'
+import { User } from './user.model'
 
-export const findLastUserId = async () => {
+export const findLastUserId = async (): Promise<string | undefined> => {
   const lastUser = await User.findOne({}, { id: 1, _id: 0 })
     .sort({
       createdAt: -1,
@@ -10,7 +10,7 @@ export const findLastUserId = async () => {
   return lastUser?.id
 }
 
-export const generateUserId = async () => {
+export const generateUserId = async (): Promise<string> => {
   const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0') //00000
   //increment by 1
   const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
