87e87c98d12fa093dac198f42bd34280795131f4
saifur2233 <saifurnstuiit223344@gmail.com>
2023-06-12 11:00:08 +0600

    [Done] Academic Semester module

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
-import { UserRoutes } from './app/modules/users/user.route'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import routes from './app/routes/index'
+import httpStatus from 'http-status'
 
 const app: Application = express()
 
@@ -12,8 +13,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Application routes
+app.use('/api/v1/', routes)
 
-app.use('/api/v1/users/', UserRoutes)
 //Testing
 app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully')
@@ -21,5 +22,20 @@ app.get('/', async (req: Request, res: Response) => {
 
 //global error handler
 app.use(globalErrorHandler)
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  })
+  next()
+})
 
 export default app
---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import validateRequest from '../../middlewares/validateRequest'
+import { AcademicSemesterController } from './academicSemester.controller'
+import { AcademicSemesterValidation } from './acdemicSemester.validation'
+const router = express.Router()
+
+router.post(
+  '/create-semester',
+  validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  AcademicSemesterController.createSemester
+)
+
+router.get('/:id', AcademicSemesterController.getSingleSemester)
+
+// router.patch(
+//   '/:id',
+//   validateRequest(AcademicSemesterValidation.updateAcademicSemesterZodSchema),
+//   AcademicSemesterController.updateSemester
+// )
+
+router.delete('/:id', AcademicSemesterController.deleteSemester)
+
+router.get('/', AcademicSemesterController.getAllSemesters)
+
+export const AcademicSemesterRoutes = router
