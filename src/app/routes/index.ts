70e0b103d6cc03b1c5052dfdeb4ad7ecdf0cd0a4
saifur2233 <saifurnstuiit223344@gmail.com>
2023-06-16 14:52:34 +0600

    [Done] Create student module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes'
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route'
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route'
 import { UserRoutes } from '../modules/user/user.route'
+import { StudentRoutes } from '../modules/student/student.route'
 
 const router = express.Router()
 
@@ -12,6 +16,18 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: academicDepartmentRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
   },
 ]
 
