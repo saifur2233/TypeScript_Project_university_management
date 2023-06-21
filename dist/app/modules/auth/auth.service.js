b7b32893f01d334fad5fa58c458440ce6b4a0713
saifur2233 <saifurnstuiit223344@gmail.com>
2023-06-21 14:17:43 +0600

    admin, faculty, auth module added

--- dist/app/modules/auth/auth.service.js
+++ dist/app/modules/auth/auth.service.js
@@ -1,74 +1,133 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict'
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthService = void 0;
-const http_status_1 = __importDefault(require("http-status"));
-const config_1 = __importDefault(require("../../../config"));
-const ApiError_1 = __importDefault(require("../../../errors/ApiError"));
-const jwtHelpers_1 = require("../../../helpers/jwtHelpers");
-const user_model_1 = require("../user/user.model");
-const loginUser = (payload) => __awaiter(void 0, void 0, void 0, function* () {
-    const { id, password } = payload;
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
+  }
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.AuthService = void 0
+const http_status_1 = __importDefault(require('http-status'))
+const config_1 = __importDefault(require('../../../config'))
+const ApiError_1 = __importDefault(require('../../../errors/ApiError'))
+const jwtHelpers_1 = require('../../../helpers/jwtHelpers')
+const user_model_1 = require('../user/user.model')
+const loginUser = payload =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const { id, password } = payload
     // creating instance of User
     // const user = new User();
     //  // access to our instance methods
     //   const isUserExist = await user.isUserExist(id);
-    const isUserExist = yield user_model_1.User.isUserExist(id);
+    const isUserExist = yield user_model_1.User.isUserExist(id)
     if (!isUserExist) {
-        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, 'User does not exist');
+      throw new ApiError_1.default(
+        http_status_1.default.NOT_FOUND,
+        'User does not exist'
+      )
     }
-    if (isUserExist.password &&
-        !(yield user_model_1.User.isPasswordMatched(password, isUserExist.password))) {
-        throw new ApiError_1.default(http_status_1.default.UNAUTHORIZED, 'Password is incorrect');
+    if (
+      isUserExist.password &&
+      !(yield user_model_1.User.isPasswordMatched(
+        password,
+        isUserExist.password
+      ))
+    ) {
+      throw new ApiError_1.default(
+        http_status_1.default.UNAUTHORIZED,
+        'Password is incorrect'
+      )
     }
     //create access token & refresh token
-    const { id: userId, role, needsPasswordChange } = isUserExist;
-    const accessToken = jwtHelpers_1.jwtHelpers.createToken({ userId, role }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
-    const refreshToken = jwtHelpers_1.jwtHelpers.createToken({ userId, role }, config_1.default.jwt.refresh_secret, config_1.default.jwt.refresh_expires_in);
+    const { id: userId, role, needsPasswordChange } = isUserExist
+    const accessToken = jwtHelpers_1.jwtHelpers.createToken(
+      { userId, role },
+      config_1.default.jwt.secret,
+      config_1.default.jwt.expires_in
+    )
+    const refreshToken = jwtHelpers_1.jwtHelpers.createToken(
+      { userId, role },
+      config_1.default.jwt.refresh_secret,
+      config_1.default.jwt.refresh_expires_in
+    )
     return {
-        accessToken,
-        refreshToken,
-        needsPasswordChange,
-    };
-});
-const refreshToken = (token) => __awaiter(void 0, void 0, void 0, function* () {
+      accessToken,
+      refreshToken,
+      needsPasswordChange,
+    }
+  })
+const refreshToken = token =>
+  __awaiter(void 0, void 0, void 0, function* () {
     //verify token
     // invalid token - synchronous
-    let verifiedToken = null;
+    let verifiedToken = null
     try {
-        verifiedToken = jwtHelpers_1.jwtHelpers.verifyToken(token, config_1.default.jwt.refresh_secret);
+      verifiedToken = jwtHelpers_1.jwtHelpers.verifyToken(
+        token,
+        config_1.default.jwt.refresh_secret
+      )
+    } catch (err) {
+      throw new ApiError_1.default(
+        http_status_1.default.FORBIDDEN,
+        'Invalid Refresh Token'
+      )
     }
-    catch (err) {
-        throw new ApiError_1.default(http_status_1.default.FORBIDDEN, 'Invalid Refresh Token');
-    }
-    const { userId } = verifiedToken;
+    const { userId } = verifiedToken
     // tumi delete hye gso  kintu tumar refresh token ase
     // checking deleted user's refresh token
-    const isUserExist = yield user_model_1.User.isUserExist(userId);
+    const isUserExist = yield user_model_1.User.isUserExist(userId)
     if (!isUserExist) {
-        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, 'User does not exist');
+      throw new ApiError_1.default(
+        http_status_1.default.NOT_FOUND,
+        'User does not exist'
+      )
     }
     //generate new token
-    const newAccessToken = jwtHelpers_1.jwtHelpers.createToken({
+    const newAccessToken = jwtHelpers_1.jwtHelpers.createToken(
+      {
         id: isUserExist.id,
         role: isUserExist.role,
-    }, config_1.default.jwt.secret, config_1.default.jwt.expires_in);
+      },
+      config_1.default.jwt.secret,
+      config_1.default.jwt.expires_in
+    )
     return {
-        accessToken: newAccessToken,
-    };
-});
+      accessToken: newAccessToken,
+    }
+  })
 exports.AuthService = {
-    loginUser,
-    refreshToken,
-};
+  loginUser,
+  refreshToken,
+}
--- dist/app/modules/user/user.model.js
+++ dist/app/modules/user/user.model.js
@@ -1,79 +1,114 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict'
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
+  }
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.User = void 0
 /* eslint-disable @typescript-eslint/no-this-alias */
-const bcrypt_1 = __importDefault(require("bcrypt"));
-const mongoose_1 = require("mongoose");
-const config_1 = __importDefault(require("../../../config"));
-const UserSchema = new mongoose_1.Schema({
+const bcrypt_1 = __importDefault(require('bcrypt'))
+const mongoose_1 = require('mongoose')
+const config_1 = __importDefault(require('../../../config'))
+const UserSchema = new mongoose_1.Schema(
+  {
     id: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     role: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
-        select: 0,
+      type: String,
+      required: true,
+      select: 0,
     },
     needsPasswordChange: {
-        type: Boolean,
-        default: true,
+      type: Boolean,
+      default: true,
     },
     student: {
-        type: mongoose_1.Schema.Types.ObjectId,
-        ref: 'Student',
+      type: mongoose_1.Schema.Types.ObjectId,
+      ref: 'Student',
     },
     faculty: {
-        type: mongoose_1.Schema.Types.ObjectId,
-        ref: 'Faculty',
+      type: mongoose_1.Schema.Types.ObjectId,
+      ref: 'Faculty',
     },
     admin: {
-        type: mongoose_1.Schema.Types.ObjectId,
-        ref: 'Admin',
+      type: mongoose_1.Schema.Types.ObjectId,
+      ref: 'Admin',
     },
-}, {
+  },
+  {
     timestamps: true,
     toJSON: {
-        virtuals: true,
+      virtuals: true,
     },
-});
+  }
+)
 UserSchema.statics.isUserExist = function (id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield exports.User.findOne({ id }, { id: 1, password: 1, role: 1, needsPasswordChange: 1 });
-    });
-};
+  return __awaiter(this, void 0, void 0, function* () {
+    return yield exports.User.findOne(
+      { id },
+      { id: 1, password: 1, role: 1, needsPasswordChange: 1 }
+    )
+  })
+}
 UserSchema.statics.isPasswordMatched = function (givenPassword, savedPassword) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield bcrypt_1.default.compare(givenPassword, savedPassword);
-    });
-};
+  return __awaiter(this, void 0, void 0, function* () {
+    return yield bcrypt_1.default.compare(givenPassword, savedPassword)
+  })
+}
 // User.create() / user.save()
 UserSchema.pre('save', function (next) {
-    return __awaiter(this, void 0, void 0, function* () {
-        // hashing user password
-        const user = this;
-        user.password = yield bcrypt_1.default.hash(user.password, Number(config_1.default.bycrypt_salt_rounds));
-        next();
-    });
-});
-exports.User = (0, mongoose_1.model)('User', UserSchema);
+  return __awaiter(this, void 0, void 0, function* () {
+    // hashing user password
+    const user = this
+    user.password = yield bcrypt_1.default.hash(
+      user.password,
+      Number(config_1.default.bycrypt_salt_rounds)
+    )
+    next()
+  })
+})
+exports.User = (0, mongoose_1.model)('User', UserSchema)
 // UserSchema.methods.isUserExist = async function (
 //   id: string
 // ): Promise<Partial<IUser> | null> {
